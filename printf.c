f09118c832368b41446d0dcf363bc73176d70cc7
winnie644 <fridahkemunto09@gmail.com>
2022-10-20 02:18:48 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - prints and input into the standard output
  * @format: the format string
@@ -48,4 +50,17 @@ int _printf(const char *format, ...)
 	_putchar(BUF_FLUSH);
 	va_end(ap);
 	return (sum);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ *  @buff_ind: Index at which to add next char, represents the length.* 
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
